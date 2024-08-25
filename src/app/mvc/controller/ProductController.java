8c9305cf8591be9eb1937cffda72bccd08c9ed70
teikim98 <teikim98@gmail.com>
2024-08-25 22:01:09 +0900

    Add ProductController productSelect

--- src/app/mvc/controller/ProductController.java
+++ src/app/mvc/controller/ProductController.java
@@ -1,7 +1,26 @@
 package app.mvc.controller;
 
+import app.mvc.exception.NotFoundException;
+import app.mvc.model.dto.Products;
+import app.mvc.model.service.ProductService;
+import app.mvc.view.EndView;
+import app.mvc.view.FailView;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class ProductController {
+
+	static ProductService productService = new ProductService();
+
 	public static void productSelect() {
-		
-	}
+		try {
+			List<Products> list = productService.productSelect();
+			EndView.printProductsList(list);
+		} catch (SQLException e) {
+			FailView.errorMessage(e.getMessage());
+        } catch (NotFoundException e) {
+			FailView.errorMessage(e.getMessage());
+        }
+    }
 }
