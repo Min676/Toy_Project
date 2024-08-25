85c8f728159379aca3942de6be2350cc401a9bb9
teikim98 <teikim98@gmail.com>
2024-08-25 20:35:04 +0900

    Add ProductsDAOImpl productSelect

--- src/app/mvc/model/dao/ProductsDAOImpl.java
+++ src/app/mvc/model/dao/ProductsDAOImpl.java
@@ -1,16 +1,36 @@
 package app.mvc.model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import app.mvc.model.dto.Products;
+import app.mvc.util.DbManager;
 
 public class ProductsDAOImpl implements ProductsDAO {
 
 	@Override
 	public List<Products> productSelect() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		Connection con=null;
+		PreparedStatement ps=null;
+		ResultSet rs=null;
+		List<Products> list = new ArrayList<>();
+		try {
+			con = DbManager.getConnection();
+			ps= con.prepareStatement("select * from Products order by PRODUCTS_ID");
+			rs = ps.executeQuery();
+
+			while(rs.next()) {
+				Products products  = new Products(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(5));
+				list.add(products);
+			}
+		}finally {
+			DbManager.close(con, ps, rs);
+		}
+		return list;
 	}
 
 	@Override
