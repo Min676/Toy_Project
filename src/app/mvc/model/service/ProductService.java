99c7c2bf0a0badc52dcb910b147574a29e41314c
teikim98 <teikim98@gmail.com>
2024-08-25 21:17:59 +0900

    Add ProductService productSelect

--- src/app/mvc/model/service/ProductService.java
+++ src/app/mvc/model/service/ProductService.java
@@ -1,5 +1,25 @@
 package app.mvc.model.service;
 
+import app.mvc.exception.NotFoundException;
+import app.mvc.model.dao.ProductsDAO;
+import app.mvc.model.dao.ProductsDAOImpl;
+import app.mvc.model.dto.Products;
+import java.sql.SQLException;
+import java.util.List;
+
 public class ProductService {
+
+    ProductsDAO productsDAO = new ProductsDAOImpl();
+
+    public List<Products> productSelect() throws NotFoundException, SQLException{
+        List<Products> list = productsDAO.productSelect();
+        if (list.size() == 0) {
+            throw new NotFoundException("상품이 없습니다 다음을 기약해주세요 ㅠㅠ");
+        }
+        return list;
+    }
+
+
+
 
 }
