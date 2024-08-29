e90583ec9ef6f5f94f0461c39468476b007cf682
iylee <iylee153@gmail.com>
2024-08-29 09:42:56 +0900

    이인영 /2024.08.29 / cart 분리

--- src/app/mvc/controller/CartController.java
+++ src/app/mvc/controller/CartController.java
@@ -1,12 +1,15 @@
 package app.mvc.controller;
 
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
+import app.mvc.exception.NotFoundException;
 import app.mvc.model.dto.OrderItem;
 import app.mvc.model.dto.Orders;
 import app.mvc.model.dto.Products;
 import app.mvc.model.dto.Users;
+import app.mvc.model.service.CartService;
 import app.mvc.model.service.OrderService;
 import app.mvc.model.service.ProductService;
 import app.mvc.session.Session;
@@ -17,6 +20,7 @@ import app.mvc.view.FailView;
 public class CartController {
 	private static ProductService productService = new ProductService();
 	private static OrderService orderService = new OrderService();
+	private static CartService cartService = new CartService();
 	public Users selectUser(int user_seq) {
 		Users u = null;
 		
@@ -26,31 +30,9 @@ public class CartController {
 	public static void putCart(String id, int productId, int quantity, int size, OrderItem orderItem) {
 		
 		try {
-			Products products = productService.productSelectByProductId(productId);
-			// 카트에 담아야 할 것 : 상품정보와 갯수, 사이즈(옵션?)
+			cartService.putCart(id, productId, quantity, size, orderItem);
 			
-			SessionSet ss = SessionSet.getInstance();
-			Session session = ss.get(id);
-			
-			//세션에서 장바구니 찾기
-			Map<OrderItem, Integer> cart = (Map<OrderItem,Integer>)session.getAttribute("cart"); //상품 , 수량 저장 
-			
-			//장바구니가 없으면 장바구니 생성
-			if(cart == null) { 
-				cart = new HashMap<>(); 
-				session.setAttribute("cart", cart);
-			}
-			
-			//장바구니에서 상품찾기
-			Integer oldQuantity = cart.get(orderItem);//goods는 장바구니 Map의 key
-			if(oldQuantity != null) { //장바구니에 이미 상품이 있다면
-				quantity += oldQuantity; //수량을 누적
-			}
-			
-			cart.put(orderItem, quantity); //장바구니에 상품 넣기
-			EndView.printMessage("장바구니에 담았습니다");
-			
-		} catch (Exception e) {
+		} catch (SQLException | NotFoundException e) {
 			FailView.errorMessage(e.getMessage());
 		}
 		
--- src/app/mvc/model/service/CartService.java
+++ src/app/mvc/model/service/CartService.java
@@ -1,5 +1,42 @@
 package app.mvc.model.service;
 
-public class CartService {
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
+import app.mvc.exception.NotFoundException;
+import app.mvc.model.dto.OrderItem;
+import app.mvc.model.dto.Products;
+import app.mvc.session.Session;
+import app.mvc.session.SessionSet;
+import app.mvc.view.EndView;
+
+	public class CartService {
+		ProductService productService = new ProductService();
+		public void putCart(String id, int productId, int quantity, int size, OrderItem orderItem) throws SQLException, NotFoundException {
+			Products products = productService.productSelectByProductId(productId);
+			// 카트에 담아야 할 것 : 상품정보와 갯수, 사이즈(옵션?)
+			
+			SessionSet ss = SessionSet.getInstance();
+			Session session = ss.get(id);
+			
+			//세션에서 장바구니 찾기
+			Map<OrderItem, Integer> cart = (Map<OrderItem,Integer>)session.getAttribute("cart"); //상품 , 수량 저장 
+			
+			//장바구니가 없으면 장바구니 생성
+			if(cart == null) { 
+				cart = new HashMap<>(); 
+				session.setAttribute("cart", cart);
+			}
+			
+			//장바구니에서 상품찾기
+			Integer oldQuantity = cart.get(orderItem);//goods는 장바구니 Map의 key
+			if(oldQuantity != null) { //장바구니에 이미 상품이 있다면
+				quantity += oldQuantity; //수량을 누적
+			}
+			
+			cart.put(orderItem, quantity); //장바구니에 상품 넣기
+			EndView.printMessage("장바구니에 담았습니다");			
+		}
+	
 }
